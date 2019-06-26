37cadaf2b22453b63e53322b4143751d9d4d5132
Fernando Cruz Filho <famcruzfilho@hotmail.com>
2019-06-26 19:53:00 -0300

    Configuração do swagger

--- Teste.Superdigital.Presentation.WEBAPI/Startup.cs
+++ Teste.Superdigital.Presentation.WEBAPI/Startup.cs
@@ -1,17 +1,14 @@
 ﻿using AutoMapper;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using Teste.Superdigital.Crosscutting.AutoMapper;
 using Teste.Superdigital.Crosscutting.DI;
 using Teste.Superdigital.Infraestructure.Data.Context;
+using Teste.Superdigital.Presentation.WEBAPI.Options;
 
 namespace Teste.Superdigital.Presentation.WEBAPI
 {
@@ -29,6 +26,15 @@ namespace Teste.Superdigital.Presentation.WEBAPI
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Title = "Teste.Superdigital API",
+                    Version = "v1"
+                });
+            });
+
             services.AddDbContext<DataContext>(optionsAction: option => option.UseSqlServer(Configuration.GetConnectionString(name: "ConnectionString")));
 
             (new Register()).RegisterClass(services, Configuration.GetConnectionString(name: "ConnectionString"));
@@ -36,39 +42,6 @@ namespace Teste.Superdigital.Presentation.WEBAPI
             var config = MapperHelper.CreateConfiguration();
             IMapper mapper = config.CreateMapper();
             services.AddSingleton(mapper);
-
-            #region [ Authentication ]
-
-            services
-                .AddIdentity<IdentityUser, IdentityRole>(option =>
-                {
-                    option.Password.RequireDigit = false;
-                    option.Password.RequiredLength = 6;
-                    option.Password.RequireNonAlphanumeric = false;
-                    option.Password.RequireUppercase = false;
-                    option.Password.RequireLowercase = false;
-                })
-                //.AddEntityFrameworkStores<DataContext>()
-                .AddDefaultTokenProviders();
-
-            services.AddAuthentication(option => {
-                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options => {
-                options.SaveToken = true;
-                options.RequireHttpsMetadata = true;
-                options.TokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidAudience = Configuration["Jwt:Site"],
-                    ValidIssuer = Configuration["Jwt:Site"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:SigningKey"]))
-                };
-            });
-
-            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -85,6 +58,21 @@ namespace Teste.Superdigital.Presentation.WEBAPI
             }
 
             app.UseHttpsRedirection();
+            app.UseDefaultFiles();
+
+            var swaggerOptions = new SwaggerOptions();
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+            app.UseSwagger(option =>
+            {
+                option.RouteTemplate = swaggerOptions.JsonRoute;
+            });
+
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint(swaggerOptions.UIEndpoint, swaggerOptions.Description);
+            });
+
             app.UseMvc();
             app.UseAuthentication();
         }
